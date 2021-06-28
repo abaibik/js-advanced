b7d550ff864beae01e9b159ad0fb06fb7eadc951
abaibik <abaibik@gmail.com>
2021-06-28 14:26:33 +0200

    added rendering photos

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const goods = [
-  { title: "Shirt", price: 150 },
-  { title: "Socks", price: 50 },
-  { title: "Jacket", price: 350 },
-  { title: "Shoes", price: 250 },
+  { id: 1, title: "Shirt", price: 150 },
+  { id: 2, title: "Socks", price: 50 },
+  { id: 3, title: "Jacket", price: 350 },
+  { id: 4, title: "Shoes", price: 250 },
 ];
 
-const renderGoodsItem = (image, title, price, link) => {
+const renderGoodsItem = (title, price, image) => {
   return `<div class="goods-item">
-  <img src = "https://picsum.photos/200/300?random=1.jpg" alt = "item image">
-  <h3 class = "goods-heading">${title}</h3>
-  <p class = "goods-price">${price}</p>
-  <p class = "pWithButton"><a class = "goods-cartButton" href = "#">Add to cart</a></p>
+  <img src="${image}" alt="item image">
+  <h3 class="goods-heading">${title}</h3>
+  <p class="goods-price">${price}</p>
+  <p class="pWithButton"><a class="goods-cartButton" href="#">Add to cart</a></p>
   </div>`;
 };
 
 const renderGoodsList = (list = goods) => {
   let goodsList = list.map((item) =>
-    renderGoodsItem(item.img, item.title, item.price)
+    renderGoodsItem(
+      item.title,
+      item.price,
+      `https://picsum.photos/200/300?random=${item.id}.jpg`
+    )
   );
   document.querySelector(".goods-list").innerHTML = goodsList.join("");
 };
