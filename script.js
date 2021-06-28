e21b46b984d3cc8f8acef043c4dd185f82821476
abaibik <abaibik@gmail.com>
2021-06-28 14:32:54 +0200

    code is shorted

--- script.js
+++ script.js
@@ -5,23 +5,17 @@ const goods = [
   { id: 4, title: "Shoes", price: 250 },
 ];
 
-const renderGoodsItem = (title, price, image) => {
+const renderGoodsItem = (item) => {
   return `<div class="goods-item">
-  <img src="${image}" alt="item image">
-  <h3 class="goods-heading">${title}</h3>
-  <p class="goods-price">${price}</p>
+  <img src="https://picsum.photos/200/300?random=${item.id}.jpg" alt="item image">
+  <h3 class="goods-heading">${item.title}</h3>
+  <p class="goods-price">${item.price}</p>
   <p class="pWithButton"><a class="goods-cartButton" href="#">Add to cart</a></p>
   </div>`;
 };
 
 const renderGoodsList = (list = goods) => {
-  let goodsList = list.map((item) =>
-    renderGoodsItem(
-      item.title,
-      item.price,
-      `https://picsum.photos/200/300?random=${item.id}.jpg`
-    )
-  );
+  const goodsList = list.map(renderGoodsItem);
   document.querySelector(".goods-list").innerHTML = goodsList.join("");
 };
 
