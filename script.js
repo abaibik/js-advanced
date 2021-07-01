1435b1beeda7e0219427991b6a962acf0415571a
abaibik <abaibik@gmail.com>
2021-07-01 18:35:09 +0200

    script of eshop modified

--- script.js
+++ script.js
@@ -1,22 +1,72 @@
-const goods = [
-  { id: 1, title: "Shirt", price: 150 },
-  { id: 2, title: "Socks", price: 50 },
-  { id: 3, title: "Jacket", price: 350 },
-  { id: 4, title: "Shoes", price: 250 },
-];
+class GoodsItem {
+  constructor(id, title, price) {
+    this.id = id;
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `<div class="goods-item">
+    <img src="https://picsum.photos/200/300?random=${this.id}.jpg" alt="item image">
+    <h3 class="goods-heading">${this.title}</h3>
+    <p class="goods-price">${this.price}</p>
+    <p class="pWithButton"><a class="goods-cartButton" href="#">Add to cart</a></p>
+    </div>`;
+  }
+}
 
-const renderGoodsItem = (item) => {
-  return `<div class="goods-item">
-  <img src="https://picsum.photos/200/300?random=${item.id}.jpg" alt="item image">
-  <h3 class="goods-heading">${item.title}</h3>
-  <p class="goods-price">${item.price}</p>
-  <p class="pWithButton"><a class="goods-cartButton" href="#">Add to cart</a></p>
-  </div>`;
-};
+class GoodsList {
+  constructor() {
+    this.goods = [];
+  }
 
-const renderGoodsList = (list = goods) => {
-  const goodsList = list.map(renderGoodsItem);
-  document.querySelector(".goods-list").innerHTML = goodsList.join("");
-};
+  fetchGoods() {
+    this.goods = [
+      { id: 1, title: "Shirt", price: 150 },
+      { id: 2, title: "Socks", price: 50 },
+      { id: 3, title: "Jacket", price: 350 },
+      { id: 4, title: "Shoes", price: 250 },
+    ];
+  }
 
-renderGoodsList();
+  render() {
+    let listHtml = "";
+    this.goods.forEach((good) => {
+      const goodItem = new GoodsItem(good.id, good.title, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector(".goods-list").innerHTML = listHtml;
+  }
+
+  count() {
+    const sum = this.goods.reduce(
+      (accumulator, good) => accumulator + good.price,
+      0
+    );
+
+    console.log(sum);
+  }
+}
+
+class cartElement extends GoodsItem {
+  constructor(id, title, price, littleImg, quantity, discount) {
+    super(id, title, price);
+    this.litleImg = littleImg;
+    this.quantity = quantity;
+    this.discount = discount;
+  }
+}
+
+class cart {
+  constructor() {
+    addToCart();
+    removeFromCart();
+    renderCart();
+    count();
+    goToCheckout();
+  }
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+list.count();
