18ce9c8452880af6f2c5c8639e5ff25f52cb3cfc
abaibik <abaibik@gmail.com>
2021-07-08 23:43:55 +0200

    lesson4

---
+++ lesson4/lesson4.js
@@ -0,0 +1,4 @@
+const textBefore =
+  "'Lorem ipsum', dolor sit 'amet consectetur' adipisicing elit. In doloremque sit reiciendis, eius officia impedit, numquam maxime voluptatum quo atque dolorum voluptatem nobis sed mollitia laboriosam sint ipsam voluptates quaerat.";
+
+const textAfter = textBefore.replace(/'/g, '"');
