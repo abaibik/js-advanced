ed644ad1d519ff927854a3797adc2910769353a5
abaibik <abaibik@gmail.com>
2021-07-09 09:30:09 +0200

    fixed lesson4

--- lesson4/lesson4.js
+++ lesson4/lesson4.js
@@ -1,4 +1,5 @@
 const textBefore =
-  "'Lorem ipsum', dolor sit 'amet consectetur' adipisicing elit. In doloremque sit reiciendis, eius officia impedit, numquam maxime voluptatum quo atque dolorum voluptatem nobis sed mollitia laboriosam sint ipsam voluptates quaerat.";
+  "'Lorem ipsum', aren't don't dolor sit 'amet consectetur' adipisicing elit. In doloremque sit reiciendis, eius officia impedit, numquam maxime voluptatum quo atque dolorum voluptatem nobis sed mollitia laboriosam sint ipsam voluptates quaerat.";
 
-const textAfter = textBefore.replace(/'/g, '"');
+// const textAfter = textBefore.replace(/'/g, '"');
+const textAfter = textBefore.replace(/\B'/g, '"').replace(/'\B/g, '"');
