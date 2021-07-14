04495aa2cfc813b43c154387950d16dc18dff4d4
abaibik <abaibik@gmail.com>
2021-07-14 21:15:33 +0200

    task 5 modified

--- script.js
+++ script.js
@@ -1,56 +1,6 @@
 const API_URL =
   "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
 
-const app = new Vue({
-  el: "#app",
-
-  data: {
-    goods: [],
-    filteredGoods: [],
-    searchLine: "",
-  },
-
-  methods: {
-    makeGETRequest(url) {
-      const promise = new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-        xhr.send();
-        xhr.onreadystatechange = function () {
-          if (xhr.readyState === 4) {
-            if (xhr.status !== 200) {
-              reject("Error");
-            } else {
-              const goods = JSON.parse(xhr.responseText).map((parsed) => {
-                return new GoodsItem(
-                  parsed.id_product,
-                  parsed.product_name,
-                  parsed.price
-                );
-              });
-              resolve(goods);
-            }
-          }
-        };
-      });
-      return promise;
-    },
-
-    onSearch() {
-      this.filteredGoods = this.goods.filter((good) =>
-        good.title.toLowerCase().includes(this.searchLine.toLowerCase())
-      );
-    },
-  },
-
-  mounted() {
-    this.makeGETRequest(`${API_URL}/catalogData.json`).then((goods) => {
-      this.goods = goods;
-      this.filteredGoods = goods;
-    });
-  },
-});
-
 class GoodsItem {
   constructor(id, title, price) {
     this.id = id;
@@ -97,3 +47,69 @@ class Cart {
     });
   }
 }
+
+const app = new Vue({
+  el: "#app",
+
+  data: {
+    goods: [],
+    filteredGoods: [],
+    searchLine: "",
+    cart: new Cart(),
+  },
+
+  methods: {
+    makeGETRequest(url) {
+      const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.send();
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+            if (xhr.status !== 200) {
+              reject("Error");
+            } else {
+              const goods = JSON.parse(xhr.responseText).map((parsed) => {
+                return new GoodsItem(
+                  parsed.id_product,
+                  parsed.product_name,
+                  parsed.price
+                );
+              });
+              resolve(goods);
+            }
+          }
+        };
+      });
+      return promise;
+    },
+
+    onSearch() {
+      this.filteredGoods = this.goods.filter((good) =>
+        good.title.toLowerCase().includes(this.searchLine.toLowerCase())
+      );
+
+      const dlg = document.querySelector("#modal");
+      dlg.modal({});
+    },
+
+    addToCart(goodsItem) {
+      const foundElement = this.goods.find((cartElement) => {
+        return cartElement.goodsItem === goodsItem;
+      });
+      if (foundElement === undefined) {
+        const cartElement = new CartElement(goodsItem);
+        this.cart.push(cartElement);
+      } else {
+        foundElement.quantity++;
+      }
+    },
+  },
+
+  mounted() {
+    this.makeGETRequest(`${API_URL}/catalogData.json`).then((goods) => {
+      this.goods = goods;
+      this.filteredGoods = goods;
+    });
+  },
+});
