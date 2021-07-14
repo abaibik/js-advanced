ecc461b03e2044e991b67587167edd81bc687211
abaibik <abaibik@gmail.com>
2021-07-14 14:15:35 +0200

    task 5-1 and 5-3

--- script.js
+++ script.js
@@ -1,85 +1,61 @@
-const makeGETRequest = (url) => {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.send();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status !== 200) {
-          reject("Error");
-        } else {
-          resolve(xhr.responseText);
-        }
-      }
-    };
-  });
-  return promise;
-};
+const API_URL =
+  "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
+
+const app = new Vue({
+  el: "#app",
+
+  data: {
+    goods: [],
+    filteredGoods: [],
+    searchLine: "",
+  },
+
+  methods: {
+    makeGETRequest(url) {
+      const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.send();
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+            if (xhr.status !== 200) {
+              reject("Error");
+            } else {
+              const goods = JSON.parse(xhr.responseText).map((parsed) => {
+                return new GoodsItem(
+                  parsed.id_product,
+                  parsed.product_name,
+                  parsed.price
+                );
+              });
+              resolve(goods);
+            }
+          }
+        };
+      });
+      return promise;
+    },
+
+    onSearch() {
+      this.filteredGoods = this.goods.filter((good) =>
+        good.title.toLowerCase().includes(this.searchLine.toLowerCase())
+      );
+    },
+  },
+
+  mounted() {
+    this.makeGETRequest(`${API_URL}/catalogData.json`).then((goods) => {
+      this.goods = goods;
+      this.filteredGoods = goods;
+    });
+  },
+});
 
 class GoodsItem {
   constructor(id, title, price) {
     this.id = id;
     this.title = title;
     this.price = price;
-  }
-  render() {
-    return `<div class="goods-item">
-    <img src="https://picsum.photos/200/300?random=${this.id}.jpg" alt="item image">
-    <h3 class="goods-heading">${this.title}</h3>
-    <p class="goods-price">${this.price}</p>
-    <p class="pWithButton"><a class="goods-cartButton" href="#">Add to cart</a></p>
-    </div>`;
-  }
-}
-
-const API_URL =
-  "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
-
-class GoodsList {
-  constructor() {
-    this.goods = [];
-    this.fetchGoods().then(() => {
-      this.render();
-    });
-  }
-
-  fetchGoods() {
-    const promise = new Promise((resolve, reject) => {
-      makeGETRequest(`${API_URL}/catalogData.json`)
-        .then((goods) => {
-          this.goods = JSON.parse(goods).map((parsed) => {
-            return new GoodsItem(
-              parsed.id_product,
-              parsed.product_name,
-              parsed.price
-            );
-          });
-          console.log("Fetch:", this.goods);
-          resolve();
-        })
-        .catch((e) => {
-          reject(e);
-        });
-    });
-
-    return promise;
-  }
-
-  render() {
-    let listHtml = "";
-    console.log("render = ", this.goods);
-    this.goods.forEach((good) => {
-      const goodItem = new GoodsItem(good.id, good.title, good.price);
-      listHtml += goodItem.render();
-    });
-    document.querySelector(".goods-list").innerHTML = listHtml;
-  }
-
-  count() {
-    return this.goods.reduce(
-      (accumulator, good) => accumulator + good.price,
-      0
-    );
   }
 }
 
@@ -121,5 +97,3 @@ class Cart {
     });
   }
 }
-
-const list = new GoodsList();
