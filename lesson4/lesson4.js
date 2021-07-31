71e116cebe7f43b6a8fc9df1ec78a65429ff4672
abaibik <abaibik@gmail.com>
2021-07-31 20:10:14 +0200

    changed text

--- lesson4/lesson4.js
+++ lesson4/lesson4.js
@@ -1,5 +1,23 @@
-const textBefore =
-  "'Lorem ipsum', aren't don't dolor sit 'amet consectetur' adipisicing elit. In doloremque sit reiciendis, eius officia impedit, numquam maxime voluptatum quo atque dolorum voluptatem nobis sed mollitia laboriosam sint ipsam voluptates quaerat.";
+const textBefore = `
+One: 'Hi Mary.' Two: 'Oh, hi.'
+One: 'How are you doing?'
+Two: 'I'm doing alright. How about you?'
+One: 'Not too bad. The weather is great isn't it?'
+Two: 'Yes. It's absolutely beautiful today.'
+One: 'I wish it was like this more frequently.'
+Two: 'Me too.'
+One: 'So where are you going now?'
+Two: 'I'm going to meet a friend of mine at the department store'
+One: 'Going to do a little shopping?'
+Two: 'Yeah, I have to buy some presents for my parents.'
+One: 'What's the occasion?'
+Two: 'It's their anniversary.'
+One: 'That's great. Well, you better get going. You don't want to be late.'
+Two: 'I'll see you next time.'
+One: 'Sure.' Bye.'
+  `;
 
 // const textAfter = textBefore.replace(/'/g, '"');
 const textAfter = textBefore.replace(/\B'/g, '"').replace(/'\B/g, '"');
+
+console.log(textAfter);
