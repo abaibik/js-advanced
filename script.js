a2b3b0801d2fb2ad23b15b28a75be7795522ad71
abaibik <78874362+abaibik@users.noreply.github.com>
2021-08-19 22:19:04 +0200

    Lesson5 (#8)
    * task 5-1 and 5-3
    
    * task 5 modified
    
    * goods item component
    
    * added component for goods
    
    * cart item added
    
    * id of photo fixed
    
    * cart ui

--- script.js
+++ script.js
@@ -1,85 +1,11 @@
-const makeGETRequest = (url) => {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.send();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status !== 200) {
-          reject("Error");
-        } else {
-          resolve(xhr.responseText);
-        }
-      }
-    };
-  });
-  return promise;
-};
+const API_URL =
+  "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
 
 class GoodsItem {
   constructor(id, title, price) {
     this.id = id;
     this.title = title;
     this.price = price;
-  }
-  render() {
-    return `<div class="goods-item">
-    <img src="https://picsum.photos/200/300?random=${this.id}.jpg" alt="item image">
-    <h3 class="goods-heading">${this.title}</h3>
-    <p class="goods-price">${this.price}</p>
-    <p class="pWithButton"><a class="goods-cartButton" href="#">Add to cart</a></p>
-    </div>`;
-  }
-}
-
-const API_URL =
-  "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
-
-class GoodsList {
-  constructor() {
-    this.goods = [];
-    this.fetchGoods().then(() => {
-      this.render();
-    });
-  }
-
-  fetchGoods() {
-    const promise = new Promise((resolve, reject) => {
-      makeGETRequest(`${API_URL}/catalogData.json`)
-        .then((goods) => {
-          this.goods = JSON.parse(goods).map((parsed) => {
-            return new GoodsItem(
-              parsed.id_product,
-              parsed.product_name,
-              parsed.price
-            );
-          });
-          console.log("Fetch:", this.goods);
-          resolve();
-        })
-        .catch((e) => {
-          reject(e);
-        });
-    });
-
-    return promise;
-  }
-
-  render() {
-    let listHtml = "";
-    console.log("render = ", this.goods);
-    this.goods.forEach((good) => {
-      const goodItem = new GoodsItem(good.id, good.title, good.price);
-      listHtml += goodItem.render();
-    });
-    document.querySelector(".goods-list").innerHTML = listHtml;
-  }
-
-  count() {
-    return this.goods.reduce(
-      (accumulator, good) => accumulator + good.price,
-      0
-    );
   }
 }
 
@@ -118,8 +44,103 @@ class Cart {
   getPrice() {
     return this.itemsList.reduce((accumulator, cartElement) => {
       return accumulator + cartElement.getPrice();
-    });
+    }, 0);
   }
 }
 
-const list = new GoodsList();
+Vue.component("base-item-img", {
+  props: ["goodId", "className"],
+  template: `<img :class="className"
+    :src="'https://picsum.photos/id/' + goodId + '/200/300'"
+    alt="item image"/>`,
+});
+
+Vue.component("goods-item", {
+  props: ["good", "cart"],
+  template: `<div class="goods-item">
+  <base-item-img :goodId="good.id"></base-item-img>
+  <h3 class="goods-heading">{{ good.title }}</h3>
+  <p class="goods-price">{{ good.price }}</p>
+  <p class="pWithButton">
+    <a
+      class="goods-cartButton"
+      v-on:click="cart.addToCart(good)"
+      href="#"
+      >Add to cart</a
+    >
+  </p>
+  </div>`,
+});
+
+Vue.component("cart-item", {
+  props: ["good", "quantity"],
+  template: `<div class="cart-item row h-50 pb-2 align-items-stretch">
+  <div class="col h-100"><base-item-img :goodId="good.id" className="h-100"></base-item-img></div>
+  <span class="goods-heading align-self-center col">{{ good.title }}</span>
+  <span class="goods-price align-self-center col">{{ good.price }}</span>
+  <span class="quantity align-self-center col">{{ quantity }}</span>
+  </div>`,
+});
+
+const app = new Vue({
+  el: "#app",
+
+  data: {
+    goods: [],
+    filteredGoods: [],
+    searchLine: "",
+    cart: new Cart(),
+  },
+
+  methods: {
+    makeGETRequest(url) {
+      const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.send();
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+            if (xhr.status !== 200) {
+              reject("Error");
+            } else {
+              const goods = JSON.parse(xhr.responseText).map((parsed) => {
+                return new GoodsItem(
+                  parsed.id_product,
+                  parsed.product_name,
+                  parsed.price
+                );
+              });
+              resolve(goods);
+            }
+          }
+        };
+      });
+      return promise;
+    },
+
+    onSearch() {
+      this.filteredGoods = this.goods.filter((good) =>
+        good.title.toLowerCase().includes(this.searchLine.toLowerCase())
+      );
+    },
+
+    addToCart(goodsItem) {
+      const foundElement = this.goods.find((cartElement) => {
+        return cartElement.goodsItem === goodsItem;
+      });
+      if (foundElement === undefined) {
+        const cartElement = new CartElement(goodsItem);
+        this.cart.push(cartElement);
+      } else {
+        foundElement.quantity++;
+      }
+    },
+  },
+
+  mounted() {
+    this.makeGETRequest(`${API_URL}/catalogData.json`).then((goods) => {
+      this.goods = goods;
+      this.filteredGoods = goods;
+    });
+  },
+});
