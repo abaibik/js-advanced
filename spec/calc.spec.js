c94b32a75a53527969a7c66608c7cf84c178b0b1
abaibik <abaibik@gmail.com>
2021-09-03 12:12:51 +0200

    data driven test

--- spec/calc.spec.js
+++ spec/calc.spec.js
@@ -1,46 +1,22 @@
 const script = require("./calc");
 const calc = script.calc;
 
-describe("calc(a, b, +)", () => {
-  it("1+1=2", () => {
-    expect(calc(1, 1, "+")).toBe(2);
-  });
+describe("calc", () => {
+  const testData = [
+    [1, 1, "+", 2],
+    [1, 2, "+", 3],
+    [1, 1, "-", 0],
+    [1, 2, "-", -1],
+    [1, 1, "*", 1],
+    [1, 0, "*", 0],
+    [4, 2, "/", 2],
+    [3, 2, "/", 1.5],
+    [3, 0, "/", Infinity],
+  ];
 
-  it("1+2=3", () => {
-    expect(calc(1, 2, "+")).toBe(3);
-  });
-});
-
-describe("calc(a,b,-)", () => {
-  it("1-1=0", () => {
-    expect(calc(1, 1, "-")).toBe(0);
-  });
-
-  it("1-2=-1", () => {
-    expect(calc(1, 2, "-")).toBe(-1);
-  });
-});
-
-describe("calc(a,b,*)", () => {
-  it("1*1=1", () => {
-    expect(calc(1, 1, "*")).toBe(1);
-  });
-
-  it("1*0=0", () => {
-    expect(calc(1, 0, "*")).toBe(0);
-  });
-});
-
-describe("calc(a,b,/)", () => {
-  it("4/2=2", () => {
-    expect(calc(4, 2, "/")).toBe(2);
-  });
-
-  it("3/2=1.5", () => {
-    expect(calc(3, 2, "/")).toBe(1.5);
-  });
-
-  it("3/0=Infinity", () => {
-    expect(calc(3, 0, "/")).toBe(Infinity);
-  });
+  for (const [a, b, op, expected] of testData) {
+    it(`calc(${a}, ${b}, ${op}) = ${expected}`, () => {
+      expect(calc(a, b, op)).toBe(expected);
+    });
+  }
 });
